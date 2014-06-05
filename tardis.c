9dc807b92d37a7a8025e7755b89b5393b032c0a8
vbhakhar <vineet3692@gmail.com>
2014-06-05 15:22:00 +0300

    Update tardis.c

--- tardis.c
+++ tardis.c
@@ -1,10 +1,12 @@
 #include "tardis.h"
 #define DEBUGMODE
+parameters* params;
+MainOptions* mainOptions;
 
 int main( int argc, char** argv)
 {
 	bam_info* in_bam;
-	parameters* params;
+	
 	configuration* cfg;
 	int return_value;
 
@@ -12,7 +14,7 @@ int main( int argc, char** argv)
 	init_params( &params);
 
 	/* Parse command line arguments */	
-	return_value = parse_command_line( argc, argv, params);
+	// return_value = parse_command_line( argc, argv, &params, &mainOptions);
 	if( return_value == 0)
 	{
 		exit( 1);
@@ -21,14 +23,14 @@ int main( int argc, char** argv)
 	#ifdef DEBUGMODE
 		print_params( params);
 	#endif
-
+	
 	/* Load configuration file */
 	cfg = ( configuration*) malloc( sizeof( configuration));
 	load_config( cfg);
 
 	/* Read BAM file and calculate the median/avg/std of fragment sizes */
 	in_bam = ( bam_info*) malloc( sizeof( bam_info));  
-	load_bam( in_bam, params->bam_file);
+	// load_bam( in_bam, params->bam_file);
 
 	/* BAM is loaded, min/max/avg/std are calculated. Now, extract FASTQs of discordants, OEAs, and orphans */
 	create_fastq( in_bam, params);
@@ -41,11 +43,10 @@ int main( int argc, char** argv)
 	*/
 	
 	/* Remap with mrFAST */
-  
+  	vhprocessing(argc, argv);
 	/* to be implemented.
 	pass config (mrfast path) and FASTQ names. 
 	params->threads will be used for multithreading option of mrFAST
 	remap(params, ...); 
 	*/
-	// A useless comment
 }
