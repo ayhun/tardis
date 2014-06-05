c42ae6878fa8e6ce72ef3027f6d41e505eb79044
vbhakhar <vineet3692@gmail.com>
2014-06-05 15:06:43 +0300

    Create vhprocessing.h

---
+++ vhprocessing.h
@@ -0,0 +1,27 @@
+#ifndef _VH_PROCESSING_
+#define _VH_PROCESSING_
+
+#include "vh_common.h"
+#include "vh_commandlineparser.h"
+#include "cmdline.h"
+#include "common.h"
+
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+void printHelp();	
+void printVersion();
+//void run(char* fileName, char* chro, char* gap, char *repeat, char* init, double preProsPrune, double minSVSup, char* outputFile);
+// parameters* params;
+// MainOptions* mainOptions;
+void run(char* , char* , char* , char *, char* , double , double , char* , int);
+int vhprocessing(int argc, char** argv);
+
+
+void pruneAndNormalizeDivets(LibraryInfo* lib, double preProsPrune, int overMapLimit);
+void quitProgram(int exitCode);
+LibraryInfo* readLibraryInfos(char* libFileAdrs);
+
+
+#endif
