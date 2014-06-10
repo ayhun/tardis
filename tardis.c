ef61ba506834b3b08925b02daf14324257f03575
vbhakhar <vineet3692@gmail.com>
2014-06-10 16:37:10 +0300

    Update tardis.c

--- tardis.c
+++ tardis.c
@@ -1,12 +1,12 @@
 #include "tardis.h"
 #define DEBUGMODE
-parameters* params;
-MainOptions* mainOptions;
+
 
 int main( int argc, char** argv)
 {
 	bam_info* in_bam;
-	
+	parameters *params;
+	MainOptions mainOptions;
 	configuration* cfg;
 	int return_value;
 
@@ -14,16 +14,16 @@ int main( int argc, char** argv)
 	init_params( &params);
 
 	/* Parse command line arguments */	
-	// return_value = parse_command_line( argc, argv, &params, &mainOptions);
+	return_value = parse_command_line( argc, argv, params, &mainOptions);
 	if( return_value == 0)
 	{
 		exit( 1);
 	}
 
-	#ifdef DEBUGMODE
+	// #ifdef DEBUGMODE
 		print_params( params);
-	#endif
-	
+	// #endif
+	printf(" \n"); //temp: @VINEET
 	/* Load configuration file */
 	cfg = ( configuration*) malloc( sizeof( configuration));
 	load_config( cfg);
@@ -31,10 +31,10 @@ int main( int argc, char** argv)
 	/* Read BAM file and calculate the median/avg/std of fragment sizes */
 	in_bam = ( bam_info*) malloc( sizeof( bam_info));  
 	// load_bam( in_bam, params->bam_file);
-
+	printf(" \n"); //temp: @VINEET	
 	/* BAM is loaded, min/max/avg/std are calculated. Now, extract FASTQs of discordants, OEAs, and orphans */
 	create_fastq( in_bam, params);
-  
+	printf(" \n"); //temp: @VINEET
 	/* sort FASTQ files to match /1 and /2 reads; unless skip-sort is invoked
 	if( !( params->skip_sort))
 	{
@@ -44,6 +44,7 @@ int main( int argc, char** argv)
 	
 	/* Remap with mrFAST */
   	vhprocessing(argc, argv);
+  	printf(" \n"); //temp: @VINEET
 	/* to be implemented.
 	pass config (mrfast path) and FASTQ names. 
 	params->threads will be used for multithreading option of mrFAST
